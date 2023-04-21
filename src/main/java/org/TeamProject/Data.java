9c701e9112ac4cd5f1df6582249ca2ad240409c6
zakuu0101 <jakub.jacqes@gmail.com>
2023-04-21 20:06:10 +0200

    recreated all gui after new guidelines

--- src/main/java/org/TeamProject/Data.java
+++ src/main/java/org/TeamProject/Data.java
@@ -1,59 +1,27 @@
 package org.TeamProject;
 
 public class Data {
-    private String data1;
-    private String data2;
-    private String data3;
-    private String data4;
-    private String data5;
+    private String value;
+    private String id;
 
-    private String[] strings = new String[5];
-
-    public String getData5() {
-        return data5;
+    public Data(String value,String id){
+    this.value=value;
+    this.id=id;
+    }
+    public String getValue() {
+        return value;
     }
 
-    public String[] getStrings() {
-        return strings;
+    public void setValue(String value) {
+        this.value = value;
     }
 
-    public void setStrings(String[] strings) {
-        this.strings = strings;
+    public String getId() {
+        return id;
     }
 
-    public void setData5(String data5) {
-        this.data5 = data5;
-    }
-
-    public String getData2() {
-        return data2;
-    }
-
-    public void setData2(String data2) {
-        this.data2 = data2;
-    }
-
-    public String getData3() {
-        return data3;
-    }
-
-    public void setData3(String data3) {
-        this.data3 = data3;
-    }
-
-    public String getData4() {
-        return data4;
-    }
-
-    public void setData4(String data4) {
-        this.data4 = data4;
-    }
-
-    public String getData1() {
-        return data1;
-    }
-
-    public void setData1(String data1) {
-        this.data1 = data1;
+    public void setId(String id) {
+        this.id = id;
     }
 }
+
--- src/main/java/org/TeamProject/UartGui.java
+++ src/main/java/org/TeamProject/UartGui.java
@@ -10,35 +10,35 @@ public class UartGui {
     private JButton SENDDATAButton;
     private JButton loadDataButton;
     private JTextField textField1;
+    private JComboBox valueBox;
+    private JComboBox comboBox1;
     private JTextField textField2;
     private JTextField textField3;
     private JTextField textField4;
     private JTextField textField5;
 
     private JTextField[] textFields = {textField1, textField2, textField3, textField4, textField5};
-    
+
     public UartGui() {
         Image icon = Toolkit.getDefaultToolkit().getImage("src/main/java/org/images/image.png");
         frame.setContentPane(mainPanel);
         frame.setTitle("UART MESSENGER");
-        frame.setSize(400, 300);
+        frame.setSize(600, 200);
         frame.setVisible(true);
         frame.setIconImage(icon);
-
         frame.setFont(Font.getFont(Font.SANS_SERIF));
-        Data data = new Data();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
 
         SENDDATAButton.addActionListener(e -> {
             System.out.println("Data sent");
         });
 
         loadDataButton.addActionListener(e -> {
-            data.setStrings(Arrays.stream(textFields).map(JTextField::getText).toArray(String[]::new));
 
-            for (String s : data.getStrings()) {
-                System.out.println(s);
-            }
+
+
         });
     }
 }
